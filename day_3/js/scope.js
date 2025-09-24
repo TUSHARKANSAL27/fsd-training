b8749fd713e741da2c4c3573fa8bb32fe627fd1b
TUSHARKANSAL27 <kansalt18@gmail.com>
2025-09-24 15:32:44 +0530

    first commit

---
+++ day_3/js/scope.js
@@ -0,0 +1,10 @@
+console.log(fun);
+function fun(){
+    console.log(name);
+    var name = "Rohini";
+    name = "Mittal"
+    console.log(name);    
+    return name;
+}
+console.log(fun(),"from invokation");
+
